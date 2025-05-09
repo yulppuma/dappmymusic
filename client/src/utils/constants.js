e5da4b007fcde7f563a38a96aa0d8b6d3465239b
Yul Puma <yulppuma@gmail.com>
2025-05-09 01:18:03 -0700

    Removed constants from .gitignore file after removing sensitive information

---
+++ client/src/utils/constants.js
@@ -0,0 +1,6 @@
+import abi from './DMM.json';
+//import abi from './DappMyMusic.json';
+export const contractABI = abi.abi;
+export const contractAddress = '0x235a2385fFcBC15A55163bBE51f5d92c41Dbb652';
+export const spotifyClientID = '6d84ad1e773643c49c41cf00095725c3';
+//export const contractAddress = '0xAC6FD047161999f37079882f1E4fdBb475fb5969';
